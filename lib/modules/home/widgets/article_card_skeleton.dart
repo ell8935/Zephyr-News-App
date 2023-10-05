24cec56835bb70dc1e16aa60ae7a9cc2ed8a7962
Elad <ell8935@gmail.com>
2023-10-05 15:11:05 +0300

    Skeleton improved

--- lib/modules/home/widgets/article_card_skeleton.dart
+++ lib/modules/home/widgets/article_card_skeleton.dart
@@ -9,58 +9,61 @@ class ArticleCardSkeleton extends StatelessWidget {
     const Color baseColor = Color.fromARGB(255, 156, 156, 156);
     const Color highlightColor = Color.fromARGB(255, 197, 197, 197);
     return Card(
+      color: baseColor,
       clipBehavior: Clip.hardEdge,
       child: InkWell(
-        child: Column(
-          children: <Widget>[
-            Shimmer.fromColors(
-              baseColor: baseColor,
-              highlightColor: highlightColor,
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.3,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Shimmer.fromColors(
+                baseColor: baseColor,
+                highlightColor: highlightColor,
+                child: const _SkeletonContainer(heightToScreenRatio: 0.25),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Shimmer.fromColors(
-              baseColor: baseColor,
-              highlightColor: highlightColor,
-              child: Container(
+              const SizedBox(
                 height: 15,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.white,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Shimmer.fromColors(
-              baseColor: baseColor,
-              highlightColor: highlightColor,
-              child: Container(
-                height: 12,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.white,
+              Shimmer.fromColors(
+                  baseColor: baseColor,
+                  highlightColor: highlightColor,
+                  child: const _SkeletonContainer(heightToScreenRatio: 0.02)),
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Shimmer.fromColors(
-              baseColor: baseColor,
-              highlightColor: highlightColor,
-              child: Container(
-                height: 30,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.white,
+              Shimmer.fromColors(
+                  baseColor: baseColor,
+                  highlightColor: highlightColor,
+                  child: const _SkeletonContainer(heightToScreenRatio: 0.05)),
+              const SizedBox(
+                height: 5,
               ),
-            ),
-          ],
+              Shimmer.fromColors(
+                  baseColor: baseColor,
+                  highlightColor: highlightColor,
+                  child: const _SkeletonContainer(heightToScreenRatio: 0.1)),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _SkeletonContainer extends StatelessWidget {
+  final double heightToScreenRatio;
+
+  const _SkeletonContainer({
+    required this.heightToScreenRatio,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5), color: Colors.white),
+      height: MediaQuery.of(context).size.height * heightToScreenRatio,
+      width: MediaQuery.of(context).size.width,
     );
   }
 }
